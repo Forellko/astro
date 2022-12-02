76b79466d8e86529da6bdefba6e064fc78d9e949
forellko <dragonforce97q@gmail.com>
2022-12-03 01:41:31 +0300

    Add toggle to product description. Add client:load to ProductCard component when used

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,13 +1,27 @@
 import { IProduct } from '../types';
+import { useState } from 'preact/hooks';
 
 interface ProductCardProps {
   product: IProduct;
 }
 
 export default function ProductCard({ product }: ProductCardProps) {
+  const [details, setDetails] = useState(false);
+  const toggleDetails = () => setDetails((prev) => !prev);
+
   return (
     <div className={'border rounded mb-2 p-3'}>
-      <h2>{product.title}</h2>
+      <h2 className="text-lg">{product.title}</h2>
+      <p className="font-bold">{product.price}</p>
+
+      <button
+        className="border py-2 px-4 bg-yellw-400 text-white"
+        onClick={toggleDetails}
+      >
+        Toggle description
+      </button>
+
+      {details && <p>{product.description}</p>}
     </div>
   );
 }
